cd5fc0384e6076cbd9e265ee1f8c22e0cb11c9dc
Татьяна Нефедова <tatka906090isakova@yandex.ru>
2023-08-12 12:45:08 +0300

    Настроено получение токена

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -35,7 +35,7 @@ class IsSuperUserOrIsAdminOnly(permissions.BasePermission):
             request.user.is_authenticated
             and (request.user.is_superuser or request.user.role == 'admin')
         )
-    
+
 
 class IsModerator(permissions.BasePermission):
     def has_permission(self, request, view):
